98f3aa68ac69f6072dc522a9493d49521b546749
IlyaTomskikh <ilya.tomskih@gmail.com>
2023-05-08 19:42:57 +0700

    consumer-producer with input arguments

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,16 +5,28 @@ import java.util.logging.Logger;
 public class Application {
     public static void main(String[] args) {
         var logger = Logger.getLogger("Main client logger");
-        var port = 1234;
         var host = "localhost";
         logger.info("Connecting...");
-        try (var socket = new Socket(host, port)){
-            var client = new ClientProducer(socket);
-            logger.info("Connected");
-            client.start();
-            while (client.isConnected());
-        } catch (IOException e) {
-            logger.info("I/O exception occurred on connection");
+        if (args[0].equals("producer")) {
+            var port = 1234;
+            try (var socket = new Socket(host, port)) {
+                var client = new ClientProducer(socket);
+                logger.info("Connected");
+                client.start();
+                while (client.isConnected()) ;
+            } catch (IOException e) {
+                logger.info("I/O exception occurred on connection");
+            }
+        } else if (args[0].equals("consumer")) {
+            var port = 4321;
+            try (var socket = new Socket(host, port)) {
+                var client = new ClientConsumer(socket);
+                logger.info("Connected");
+                client.start();
+                while (client.isConnected()) ;
+            } catch (IOException e) {
+                logger.info("I/O exception occurred on connection");
+            }
         }
     }
 }
